2ddd48f321158acf4868fc1d8a0fb4952e8646e9
Mingrael <huntmaster636@gmail.com>
2017-02-26 18:49:58 +0300

    Getters and setters for users class

--- src/com/User.java
+++ src/com/User.java
@@ -16,5 +16,35 @@ public class User {
         this.pass = pass;
     }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
 }
