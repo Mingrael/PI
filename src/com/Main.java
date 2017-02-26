a6b395c85181f1fe10a0678633312b5c3ac986c9
Mingrael <huntmaster636@gmail.com>
2017-02-26 16:12:45 +0300

    Add User class, fill users array

---
+++ src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Миша on 26.02.2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User(1, "John Doe", "jdoe","sup3rpaZZ"));
+        users.add(new User(2, "Jane Row", "jrow","Qweqrty12"));
+    }
+}
---
+++ src/com/User.java
@@ -0,0 +1,20 @@
+package com;
+
+/**
+ * Created by Миша on 26.02.2017.
+ */
+public class User {
+
+    int id;
+    String name, login, pass;
+
+    public User(int id,String name, String login, String pass)
+    {
+        this.id = id;
+        this.name = name;
+        this.login = login;
+        this.pass = pass;
+    }
+
+
+}
